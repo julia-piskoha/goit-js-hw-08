518802796d1769528ef280b735a4d43ef25be8bc
julia-piskoha <upiskoha@gmail.com>
2022-11-28 00:36:07 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryList = makeGalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryList);
+var lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+});
 
+function makeGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
 console.log(galleryItems);
