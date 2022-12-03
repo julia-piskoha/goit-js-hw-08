7c0e31b7e0a3bb00c4fab788010944234fffde0b
julia-piskoha <upiskoha@gmail.com>
2022-12-03 16:11:58 +0200

    correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,7 @@ player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 player.on('timeupdate', throttle(currentVideoTime, 1000));
 
 function currentVideoTime({ seconds, duration }) {
-  if (seconds > duration) return localStorage.clear();
+  if ((seconds = duration)) return localStorage.clear();
 
   localStorage.setItem('videoplayer-current-time', seconds);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,32 @@
 import throttle from 'lodash.throttle';
-const Form = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
-fillForm();
+const form = document.querySelector('.feedback-form');
+
 const key = 'feedback-form-state';
-let formData = {};
-Form.form.addEventListener('input', onFormInput);
-Form.form.addEventListener('submit', throttle(onFormSubmit, 500));
+fillForm();
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(e) {
-  if (localStorage.getItem(key) === '') {
-    formData = JSON.parse(localStorage.getItem(key));
-  }
-  formData[e.target.name] = e.target.value;
-  const JSONformData = JSON.stringify(formData);
-  localStorage.setItem(key, JSONformData);
+  const storageValue = localStorage.getItem(key);
+  const usersData = storageValue ? JSON.parse(storageValue) : {};
+  usersData[e.target.name] = e.target.value;
+  localStorage.setItem(key, JSON.stringify(usersData));
 }
 function onFormSubmit(e) {
   e.preventDefault();
+  const { email, message } = e.target.elements;
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
   localStorage.removeItem(key);
   e.currentTarget.reset();
-  formData = {};
-  console.log(formData);
 }
 function fillForm() {
+  const storageValue = localStorage.getItem(key);
+  if (!storageValue) return;
   const formObject = JSON.parse(localStorage.getItem(key));
-  if (formObject === '') {
-    const { email, message } = formObject;
-    Form.email.value = email;
-    Form.textarea.value = message;
-  }
+  const { email, message } = formObject;
+  form.email.value = email;
+  form.message.value = message;
 }
